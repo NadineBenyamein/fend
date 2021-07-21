e8fd4c799640ccb4038e59782350bb892d873967
Nadine Benyamein <nadine.george@hotmail.com>
2021-07-21 16:36:36 +0200

    Navigation bar done - Navigates using scroll to sections. Not formatted yet

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,7 +17,9 @@
  * Define Global Variables
  * 
 */
-
+const sections = document.querySelectorAll('section');
+const navigationbar = document.querySelector('#navbar__list');
+const fragment = document.createDocumentFragment();
 
 /**
  * End Global Variables
@@ -35,6 +37,19 @@
 
 // build the nav
 
+sections.forEach(function(section){
+let menutext = section.getAttribute('data-nav');
+let list = document.createElement('li');
+let textnode = document.createTextNode(menutext);
+let link = document.createElement('a');
+list.addEventListener('click', function scrolling() {
+section.scrollIntoView({behavior: "smooth"});})
+link.appendChild(textnode);
+list.appendChild(link);
+fragment.appendChild(list);
+navigationbar.appendChild(fragment);
+list.style.cssText ='border: none; color: #cccc11; padding: 16px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; transition-duration: 0.4s; cursor: pointer; width: 1000 px;'
+});
 
 // Add class 'active' to section when near top of viewport
 
