79005391260d77a1595b432afd9a958f5c6bfa43
Nadine Benyamein <nadine.george@hotmail.com>
2021-07-22 00:03:53 +0200

    * added an improvement for the active section * added comments * added README.md * modified colors

--- projects/landing-page/README.md
+++ projects/landing-page/README.md
@@ -11,3 +11,8 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+## Workflow
+
+In this project, a static landing page has been converted into an interactive one with the use of JavaScript.
+A fourth section was added to the page. A navigation menu has been built as an unordered list with the names of the sections on the page, and each linking and scrolling to its section upon clicking. The page behavior has also been updated to show which section is being viewed upon scrolling.
--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -13,7 +13,7 @@
  * 
 */
 
-/**
+/*
  * Define Global Variables
  * 
 */
@@ -21,45 +21,49 @@ const sections = document.querySelectorAll('section');
 const navigationbar = document.querySelector('#navbar__list');
 const fragment = document.createDocumentFragment();
 
-/**
+/*
  * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
 */
 
 // build the nav
+
+//Function to build the navigation menu.
 sections.forEach(function(section)
 {
+    //get text from "data-nav" attribute which will be displayed on navigation menu
     let menutext = section.getAttribute('data-nav');
+    //create list element in the document
     let list = document.createElement('li');
+    //save the test from data-nav into textnodes
     let textnode = document.createTextNode(menutext);
+    //create link elements in the document
     let link = document.createElement('a');
 
+    //listen to "clicking on button" event and respond by scrolling to the desired section
     list.addEventListener('click', function scrolling() 
     {
+        //scrolling smoothly to desired section
         section.scrollIntoView({behavior: "smooth"});
     })
+    //making every variable a child of the other (textnode in link, link in list, list in fragment and fragment in navigationbar)
     link.appendChild(textnode);
     list.appendChild(link);
     fragment.appendChild(list);
     navigationbar.appendChild(fragment);
-    list.style.cssText ='border: none; color: #cccc11; padding: 16px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; transition-duration: 0.4s; cursor: pointer; width: 1000 px;'
+    //changing the style of the navigation menu
+    list.style.cssText ='border: none; color: rgb(204, 204, 17); background-color: rgb(160 9 33); padding: 16px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; transition-duration: 0.4s; cursor: pointer; width: 1000 px;'
 });
 
 
 // Add class 'active' to section when near top of viewport
+// Scroll to anchor ID using scrollTO event
+
+//this function returns whether the current section is in ViewPort or not
 function sectionPosition (currentSection){
     let isSectionInView = false;
     let position = currentSection.getBoundingClientRect();
     
+    //section is in view, if the number of pixels from the top of the element to the top of view port is >= 0. Otherwise, it's out of view.
     if(position.top >= 0)
     {
         isSectionInView = true;
@@ -67,35 +71,31 @@ function sectionPosition (currentSection){
     return(isSectionInView);
 }
 
+//this function adds the class "your-active-class" to the section in view, and removes "your-active-class" from those that left the view port.
 function activeClassSwap()
 {
+    let activeSectionFound = false;
+    //execute for every section
     for(section of sections)
     {
+        //remove "your active class" from all sections
+        section.classList.remove('your-active-class');
+
+        //check if section is in view
         if (sectionPosition(section))
-        {
-            section.classList.add('your-active-class');
-        }
-        else
-        {
-            section.classList.remove('your-active-class');
+        {   
+            //check if no other section was set as "your active class" already
+            if(activeSectionFound == false)
+            {
+                //add "your active class" to the first section in view & change activeSectionFound to "true" so that no other sections are also set as active
+                section.classList.add('your-active-class');
+                activeSectionFound = true;
+            }
+            
+            
         }
     }
 }
+//event listener to execute the active class swap function upon scrolling.
 document.addEventListener('scroll', activeClassSwap);
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
 
